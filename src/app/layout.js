ae5a056fdbe99c833c62f74173768cc57c7eaf3f
Brian <badat336@gmail.com>
2024-01-17 16:49:05 +0700

    Resolve error

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,7 +35,6 @@ export default function RootLayout({ children }) {
             <body>
                 <Header />
                 {children}
-                <Footer />
 
                 <SpeedInsights />
             </body>
